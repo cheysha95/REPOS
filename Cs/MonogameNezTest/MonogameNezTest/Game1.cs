194ce6abeb04afbd2478ff71ba7f60f84401c48d
chey <hyman.sha95@gmail.com>
2022-12-18 04:10:48 -0600

    started new porject

---
+++ Cs/MonogameNezTest/MonogameNezTest/Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+using Nez.Particles;
+using Nez.Sprites;
+using Nez.Textures;
+
+
+
+namespace MonogameNezTest
+{
+    public class Game1 : Nez.Core
+    {
+        public Game1() : base()
+        {
+
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            //----------------------------------------------------------make scene first          
+            Scene testScene = Scene.CreateWithDefaultRenderer(Color.CornflowerBlue);
+            Core.Scene = testScene;
+            //----------------------------------------------------------------- create a texture, create new entity
+            Texture2D linkPic = testScene.Content.Load<Texture2D>("sprite");
+           // Entity testEntity = Scene.CreateEntity("first_guy");
+            //------------------------------------------------------------------- add a sprtie component to entity
+            //testEntity.AddComponent(new SpriteRenderer(linkPic));
+            //---------------------------------------------------------------------move (transform) entitygive new position
+           // testEntity.Transform.SetPosition(new Vector2(300,300));
+
+
+            //create map and renderer, entities draws automatically.
+            var map = Content.LoadTiledMap("Content/easymap.tmx");
+            var tiledEntity = Scene.CreateEntity("tiled-map");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Cs/MonogameNezTest/MonogameNezTest/bin/Debug/net6.0/Content/Oracle_tileset.tsx
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Oracle_Tileset" tilewidth="8" tileheight="8" tilecount="7310" columns="86">
+ <editorsettings>
+  <export target="../../../../../../Desktop/Oracle_tileset..tsx" format="tsx"/>
+ </editorsettings>
+ <image source="Oracle_Tileset.png" width="688" height="680"/>
+ <tile id="89">
+  <animation>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="90" duration="200"/>
+   <frame tileid="91" duration="400"/>
+   <frame tileid="90" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="180">
+  <animation>
+   <frame tileid="180" duration="100"/>
+   <frame tileid="182" duration="100"/>
+   <frame tileid="184" duration="100"/>
+   <frame tileid="182" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="181">
+  <animation>
+   <frame tileid="181" duration="100"/>
+   <frame tileid="183" duration="100"/>
+   <frame tileid="185" duration="100"/>
+   <frame tileid="183" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="266">
+  <animation>
+   <frame tileid="266" duration="100"/>
+   <frame tileid="268" duration="100"/>
+   <frame tileid="270" duration="100"/>
+   <frame tileid="268" duration="100"/>
+   <frame tileid="266" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="267">
+  <animation>
+   <frame tileid="267" duration="100"/>
+   <frame tileid="269" duration="100"/>
+   <frame tileid="271" duration="100"/>
+   <frame tileid="269" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="352">
+  <animation>
+   <frame tileid="352" duration="100"/>
+   <frame tileid="354" duration="100"/>
+   <frame tileid="356" duration="100"/>
+   <frame tileid="354" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="353">
+  <animation>
+   <frame tileid="353" duration="100"/>
+   <frame tileid="355" duration="100"/>
+   <frame tileid="357" duration="100"/>
+   <frame tileid="355" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="438">
+  <animation>
+   <frame tileid="438" duration="100"/>
+   <frame tileid="440" duration="100"/>
+   <frame tileid="442" duration="100"/>
+   <frame tileid="440" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="439">
+  <animation>
+   <frame tileid="439" duration="100"/>
+   <frame tileid="441" duration="100"/>
+   <frame tileid="443" duration="100"/>
+   <frame tileid="441" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
